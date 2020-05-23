3c5c6188b2d3cdd3eaae4af398764a495de5075e
Chan Davis <chandlerfdavis@gmail.com>
2020-05-23 18:22:11 -0500

    Add CSS and basic grid changing functionality
    - The app now uses JQuery, which is pull from the Google CDN.
    - Added a (partially-populated) map containing selectable flowers and their colors.
      - The character represent the flowers are the keys and the values are object containing the
        flower name and an array of possible colors for that flower.
      - Currently, you can select flowers by pressing the keys: T, L, P, W
      - Use the left and right arrow keys to cycle through the possible flower colors
    - If you click a cell in the grid, that cell will get the new flower character and background color.

---
+++ ACNH-Flower-Breeding-Sim.js
@@ -0,0 +1,109 @@
+var gridHeight = 5; /* Number of rows in the grid. */
+var gridWidth = 5; /* Number of columns in the grid. */
+
+var selectedFlower = 't';
+var flowerColor = 'blue';
+var flowerColorIndex = 0;
+
+const flowerColorMap = new Map();
+flowerColorMap.set('t', 
+{
+   'name': 'tulip',
+   'colors': ['red', 'yellow', 'orange', 'white', 'pink', 'purple', 'black']
+});
+
+flowerColorMap.set('w',
+{
+   'name': 'windflower',
+   'colors': ['read', 'orange', 'white', 'blue', 'pink', 'purple']
+});
+
+flowerColorMap.set('l',
+{
+   'name': 'lily',
+   'colors': ['red', 'orange', 'yellow', 'black', 'pink', 'white']
+});
+
+flowerColorMap.set('p',
+{
+   'name': 'pansy',
+   'colors': ['red', 'orange', 'yellow', 'blue', 'white', 'purple']
+});
+
+
+function updateGridSize () {
+   // TODO: Add titles for which input field is width/height
+
+   // Get the new values and update the global variables.
+   var newHeight = document.getElementById("grid-height-input").value;
+   var newWidth = document.getElementById("grid-width-input").value;
+   gridHeight = newHeight;
+   gridWidth = newWidth;
+
+   // Create and fill the grid.
+   createGrid();
+}
+
+function createGrid () {
+   var grid = document.getElementById("grid");
+   grid.innerHTML = "";
+
+   for (var row = 0; row < gridHeight; row++) {
+      let newTableRow = document.createElement("tr");
+      for (var col = 0; col < gridWidth; col++) {
+         let newRowCell = document.createElement("td");
+         newRowCell.innerHTML = "W";
+         newTableRow.appendChild(newRowCell);
+      }
+      grid.appendChild(newTableRow);
+   }
+}
+
+// Gets keyDown events and calls the appropriate function based on pressed key.
+function keyUpHandler (event) {
+   switch (event.key) {
+      case 'ArrowLeft':
+         cycleColor(-1);
+         break;
+      case 'ArrowRight':
+         cycleColor(1);
+         break;
+      default:
+         selectFlower(event.key);
+   }
+}
+
+// Sets the currently selected flower if the given selection is a valid flower type.
+function selectFlower (choice) {
+   if (flowerColorMap.has(choice)) {
+      selectedFlower = choice;
+      flowerColorIndex = 0;
+      console.log('New Flower Selected: ', flowerColorMap.get(selectedFlower).name); 
+   }
+}
+
+// Cycles through the color list for the currently selected flower.
+function cycleColor (direction) {
+   let curFlowerColors = flowerColorMap.get(selectedFlower).colors;
+   flowerColorIndex += direction;
+   flowerColorIndex = (flowerColorIndex >= curFlowerColors.length)? 0 : flowerColorIndex;
+   flowerColorIndex = (flowerColorIndex < 0)? curFlowerColors.length - 1 : flowerColorIndex;
+
+   flowerColor = curFlowerColors[flowerColorIndex];
+
+   // TODO: Remove later
+   console.log(curFlowerColors);
+   console.log(flowerColor);
+}
+
+/* JQuery Code */
+$(document).ready(function () {
+   
+   $(document).keyup(keyUpHandler);
+
+   $("table#grid").on("click", "td", function (event) {
+      $(this).css("background-color", flowerColor);
+      $(this).text(selectedFlower.toUpperCase());
+   });
+
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+### Possible Ideas:
+* Enable selection of entire rows or entire columns
+* Saving/Loading from a file
+* Saving current field state when updating the size
